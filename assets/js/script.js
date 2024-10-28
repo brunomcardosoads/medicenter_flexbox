b5a99f9ee56157a9ba1b29f72f497a5e18d989b1
brunomcardosoads <brunomoraiscard@hotmail.com>
2024-10-28 09:40:42 -0300

    subindo o projeto landing page MediCenter

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    document.querySelector(".menuMobile").addEventListener("click", function() {
+        if(document.querySelector(".menu nav ul").style.display == 'flex') {
+            document.querySelector(".menu nav ul").style.display = 'none';
+        } else {
+            document.querySelector(".menu nav ul").style.display = 'flex';
+        }
+    });
+    
+window.addEventListener('resize', function () {
+    var largura = window.innerWidth;
+
+    if (largura > 1020)
+        document.querySelector(".menu nav ul").removeAttribute('style');
+    });
+};
